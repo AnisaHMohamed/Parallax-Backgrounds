ab4e0a59bb127f554a0d9c8bae727517fb815b45
AnisaHMohamed <anisahamohamed@gmail.com>
2022-08-01 11:14:20 -0400

    parllax effect

--- script.js
+++ script.js
@@ -15,17 +15,46 @@ backgroundLayer4.src = "./backgroundLayers/layer-4.png"
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = "./backgroundLayers/layer-5.png"
 
-let x = 0;
-// starts where first image ends
-let x2 = 2400;
+class Layer {
+    constructor(image, speedModifier) {
+        this.x = 0;
+        this.y = 0;
+        this.width = 2400;
+        this.height = 700;
+        this.x2 = this.width;
+        this.image = image;
+        this.speedModifier = speedModifier;
+        this.speed = gamespeed * speedModifier;
+    }
+    update(){
+        this.speed = gamespeed * this.speedModifier;
+        if(this.x <= -this.width) {
+            this.x = this.width + this.x2 - this.speed;
+        }
+        if(this.x2 <= -this.width) {
+            this.x2 = this.width + this.x - this.speed;
+        }
+        this.x = Math.floor(this.x - this.speed);
+        this.x2 = Math.floor(this.x2 - this.speed);
+    }
+    draw(){
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+    }
+}
+
+const layer1 = new Layer(backgroundLayer1,0.5);
+const layer2 = new Layer(backgroundLayer2,0.5);
+const layer3 = new Layer(backgroundLayer3,0.5);
+const layer4 = new Layer(backgroundLayer4,0.5);
+const layer5 = new Layer(backgroundLayer5,2.5);
+
 const animate = () => {
-    ctx.clearRect(0, 0,CANVAS_WIDTH,CANVAS_HEIGHT)
-    ctx.drawImage(backgroundLayer4,x,0)
-    ctx.drawImage(backgroundLayer4,x2,0)
-    if(x<-2400) x = 2400 + x2 - gamespeed
-    x-= gamespeed;   
-     if(x2<-2400) x2 = 2400 + x - gamespeed
-    x2-= gamespeed;
+    ctx.clearRect(0, 0,CANVAS_WIDTH,CANVAS_HEIGHT);
+    layer4.update()
+    layer4.draw();
+    layer5.update()
+    layer5.draw();
     requestAnimationFrame(animate)
 }
 animate()
